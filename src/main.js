019f0f88ef58dd5b3aac6a2ca5e16fd7f0fc704e
yk <1586500126@qq.com>
2019-05-14 14:09:30 +0800

    项目初步开始

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入一个解决移动端300毫秒的延迟
+import fastclick from 'fastclick'
+// 通用样式
+import "./assets/style/reset.css"
+// 解决移动端1px 像素的问题
+import "./assets/style/border.css"
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
+  router:router,
+  components: { App:App },
   template: '<App/>'
 })
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <!-- <div id="app"> -->
-    <router-view></router-view>
-  <!-- </div> -->
+    <div>
+        hello world
+    </div>
 </template>
 
 <script>
 export default {
-
+    name:"Home"
 }
 </script>
-
 <style>
-
+    
 </style>
+
