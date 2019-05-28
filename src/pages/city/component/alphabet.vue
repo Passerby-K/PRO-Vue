c50e31ab14ba73a8a2bf15d9394f2074012d3f09
yk <1586500126@qq.com>
2019-05-28 17:04:59 +0800

    add city-seach

--- src/pages/city/component/alphabet.vue
+++ src/pages/city/component/alphabet.vue
@@ -29,8 +29,13 @@ export default {
     },
     data(){
         return{
-            touchstatus:false
+            touchstatus:false,
+            startY:0,
+            timer:null
         }
+    },
+    updated(){
+        this.startY=this.$refs["A"][0].offsetTop
     },
     methods:{
         handclick (e){
@@ -42,13 +47,20 @@ export default {
         },
         handmove(e){
             if(this.touchstatus){
-                const startY=this.$refs["A"][0].offsetTop
-                const touchY=e.touches[0].clientY - 79
-                const index=Math.floor((touchY-startY)/20)
-                console.log(index)
-                if(index>=0 && index<this.letters.length){
-                    this.$emit("change",this.letters[index])
+                
+                // const startY=this.$refs["A"][0].offsetTop
+                if(this.timer){
+                    clearTimeout(this.tiemr)
                 }
+                this.timer=setTimeout(()=>{
+
+                    const touchY=e.touches[0].clientY - 79
+                    const index=Math.floor((touchY-this.startY)/20)
+                    console.log(index)
+                    if(index>=0 && index<this.letters.length){
+                        this.$emit("change",this.letters[index])
+                    }
+                },16)
             }
         },
         handend(){
--- src/pages/city/component/search.vue
+++ src/pages/city/component/search.vue
@@ -1,12 +1,63 @@
 <template>
+<div>
+
   <div class="search">
-      <input type="text" class="search-inp" placeholder="请输入城市名">
+      <input type="text" v-model="keyword" class="search-inp" placeholder="请输入城市名">
   </div>
+  <div class="seach-content" ref="search" v-show="keyword">
+      <ul>
+          <li class="item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+          <li class="item border-bottom" v-show="hasList" >没有相关的数据</li>
+      </ul>
+  </div>
+</div>
+    
 </template>
 
 <script>
+import Bscroll from "better-scroll"
 export default {
-    name:"CitySearch"
+    name:"CitySearch",
+    props:{
+        cities:Object
+    },
+    data(){
+        return{
+            keyword:"",
+            list:[],
+            timer:null
+        }
+    },
+    computed:{
+        hasList(){
+            return !this.list.length
+        }
+    },
+    watch:{
+        keyword(){
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            if(!this.keyword){
+                this.list=[]
+                return
+            }
+            this.timer=setTimeout(() => {
+                const result=[]
+                for (let i in this.cities){
+                    this.cities[i].forEach((value)=>{
+                        if(value.spell.indexOf(this.keyword)>-1  || value.name.indexOf(this.keyword)>-1){
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list=result
+            }, 100);
+        }
+    },
+    mounted(){
+        this.scroll=new Bscroll(this.$refs.search)
+    }
 }
 
 </script>
@@ -24,4 +75,19 @@ export default {
             text-align center
             border-radius .06rem
             color #666
+    .seach-content
+        z-index 1
+        overflow hidden
+        position absolute
+        top 1.60rem
+        bottom 0
+        left 0
+        right 0
+        background-color #eeeeee
+        .item
+            line-height .62rem
+            padding-left .2rem
+            color #666
+            background-color #fff
+
 </style>
