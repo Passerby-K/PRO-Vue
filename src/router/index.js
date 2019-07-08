cf6a9a92a7a27daa8bd0beffb05268adc1fb253f
yk <1586500126@qq.com>
2019-07-08 15:45:50 +0800

    修改异步组件，按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/city'
-import Detail from '@/pages/detail/Detail'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/city'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,17 +11,23 @@ export default new Router({
     {
       path:"/",
       name:"Home",
-      component:Home
+      // component:Home
+      // 异步组件，按需加载
+      component:()=>import("@/pages/home/Home")
     },
     {
       path:"/city",
       name:"City",
-      component:City
+      // component:City
+      component:()=>import("@/pages/city/city")
+      
     },
     {
       path:"/detail/:id",
       name:"Detail",
-      component:Detail
+      // component:Detail
+      component:()=>import("@/pages/detail/Detail")
+      
     }
   ],
   scrollBehavior(to,from,savedPosition){
